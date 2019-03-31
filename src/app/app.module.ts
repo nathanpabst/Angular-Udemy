89213c7748a0d60627c68db9879c7881f6949546
Nathan Pabst <Nathan.Pabst@ey.com>
2019-03-31 10:45:36 -0500

    example of two-way binding in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
@@ -18,7 +18,8 @@ import { MorePracticeComponent } from './more-practice/more-practice.component';
     MorePracticeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
