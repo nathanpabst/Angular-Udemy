16613eb70279bd745816e8d78968ecdc1a636a93
Nathan Pabst <Nathan.Pabst@ey.com>
2019-04-04 16:47:04 -0500

    binding to custom events. wip

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -9,7 +9,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ServerElementComponent implements OnInit {
   //typescript syntax for defining a type. allows us to access the info in the html file.
   //@Input() decorator allows parent component to be able to bind to a property
-  @Input() element: {type: string, name: string, content: string};
+  //@Input('srvElement') ...adding an alias to a custom prop
+  @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
 
