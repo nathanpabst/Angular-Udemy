e1149c07b4c666c9b49e1360171d65072599251e
Nathan Pabst <Nathan.Pabst@ey.com>
2019-04-05 18:28:34 -0500

    using @ViewChild to access the template and DOM.

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -10,24 +10,26 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   //newServerName = ''; using local ref, so this is no longer needed
-  newServerContent = '';
-  
+  //newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer(nameInput: HTMLInputElement) {
+  onAddServer(nameInput: HTMLInputElement) {  
    this.serverCreated.emit({
      serverName: nameInput.value,
-     serverContent: this.newServerContent
+     serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
