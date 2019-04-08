ad4deea9f3a8871b731cd36daee7c1520864b1a5
Nathan Pabst <Nathan.Pabst@ey.com>
2019-04-07 19:08:09 -0500

    example of a basic attribute directive in place.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ServerElementComponent } from './server-element/server-element.componen
 import { GameControlComponent } from './game-control/game-control.component';
 import { EvenComponent } from './even/even.component';
 import { OddComponent } from './odd/odd.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { OddComponent } from './odd/odd.component';
     ServerElementComponent,
     GameControlComponent,
     EvenComponent,
-    OddComponent
+    OddComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
